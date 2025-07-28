7241df34fd6b5fe84f16b30d53ba73a8aef87d0a
Nitin Tanwar <166863746+Nitin23123@users.noreply.github.com>
2025-07-28 21:34:42 +0530

    Add files via upload

---
+++ sortcolor.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0 , count1= 0, count2= 0;
+        for (int i = 0 ;i<n ;i++){
+            if(nums[i]==0){
+                count++;
+            }
+            else if( nums[i]==1) count1++;
+            else count2++;
+        }
+        int index = 0;
+        for(int i = 0 ; i<count ; i++){
+            nums[index++] = 0;
+        }
+        for(int i = 0 ; i<count1 ; i++){
+            nums[index++] = 1;
+        }
+        for(int i = 0 ; i<count2 ; i++){
+            nums[index++] = 2;
+        }
+        for(int i = 0 ; i<n ; i++){
+            cout<<nums[i];
+        }
+    }
+};
